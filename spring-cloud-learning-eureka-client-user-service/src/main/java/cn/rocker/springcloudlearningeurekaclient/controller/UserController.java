e4938077606112f0b9f71dbcdd4a1d4bea88a8ed
rocker <rocker_pg@163.com>
2019-09-16 23:48:30 +0800

    vvv

--- spring-cloud-learning-eureka-client-user-service/src/main/java/cn/rocker/springcloudlearningeurekaclient/controller/UserController.java
+++ spring-cloud-learning-eureka-client-user-service/src/main/java/cn/rocker/springcloudlearningeurekaclient/controller/UserController.java
@@ -8,15 +8,15 @@ import org.springframework.web.bind.annotation.*;
  * @since V1.0
  */
 @RestController
-public class UserController {
+public class UserController /*implements UserRemoteClient */{
 
     @GetMapping("/user/hello")
     public String hello(@RequestParam("randomNum") Integer randomNum){
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            Thread.sleep(999);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
 //        if(randomNum % 2 == 0){
 //            throw new RuntimeException("来了个偶数，真新鲜");
 //        }
